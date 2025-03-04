e9725a665d856e72c3a7fda2e40d80ef39c5fac7
L-pixela <122270958+L-pixela@users.noreply.github.com>
2025-03-04 23:33:31 +0700

    BLA-101 – Refactor Location Service

--- blacar_part2/lib/service/locations_service.dart
+++ blacar_part2/lib/service/locations_service.dart
@@ -1,6 +1,7 @@
 import 'package:week_3_blabla_project/model/ride/locations.dart';
 
 import '../dummy_data/dummy_data.dart';
+import '../repository/locations_repository.dart';
 
 ////
 ///   This service handles:
@@ -8,4 +9,59 @@ import '../dummy_data/dummy_data.dart';
 ///
 class LocationsService {
   static const List<Location> availableLocations = fakeLocations;
+
+  //static prive instance
+  static LocationsService? _instance;
+
+  // Access to past locations
+  final LocationsRepository repository;
+
+  Location? _currentLocations;
+
+  ///
+  /// Private Constructor
+  ///
+  LocationsService._internal(this.repository);
+
+  ///
+  /// Initialize
+  ///
+  static void initialize(LocationsRepository repository) {
+    if (_instance == null) {
+      _instance = LocationsService._internal(repository);
+    } else {
+      throw Exception("Already Initialized"); // Already initialized
+    }
+  }
+
+  ///
+  /// Singleton accessor
+  ///
+  static LocationsService get instance {
+    if (_instance == null) {
+      throw Exception(
+          "RidePreferencesService is not initialized. Call initialize() first.");
+    }
+    return _instance!;
+  }
+
+  // Current preference
+  Location? get currentLocations {
+    print('Get  current  Locations : $_currentLocations');
+    return _currentLocations;
+  }
+
+  void setCurrentPreference(Location location) {
+    _currentLocations = location;
+    print('Set current Locations to $_currentLocations');
+  }
+
+  // Locations
+  List<Location> getLocations() {
+    return repository.getLocations();
+  }
+
+  void addLocation(Location location) {
+    return repository.addLocations(location);
+  }
 }
